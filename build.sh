41fd36d19997219fb73cdb91c8720522b458b056
Bruce Becker <brucellino@gmail.com>
2016-11-24 12:25:22 +0100

    Added build.sh for r-lang

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+
+module add ci
+module add zlib
+module add openblas/0.2.15-gcc-5.1.0
+module add lapack/3.6.0-gcc-5.1.0
+module add jdk/8u66
+module add ncurses
+module add readline/6.3
+
+SOURCE_FILE=${NAME}-${VERSION}.tar.bz2
+
+echo "REPO_DIR is "
+echo $REPO_DIR
+echo "SRC_DIR is "
+echo $SRC_DIR
+echo "WORKSPACE is "
+echo $WORKSPACE
+echo "SOFT_DIR is"
+echo $SOFT_DIR
+
+mkdir -p ${WORKSPACE}
+mkdir -p ${SRC_DIR}
+mkdir -p ${SOFT_DIR}
+
+#  Download the source file
+
+if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
+  touch  ${SRC_DIR}/${SOURCE_FILE}.lock
+  echo "seems like this is the first build - let's geet the source"
+  wget http://mirror.ufs.ac.za/gnu/gnu/gmp/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  echo "releasing lock"
+  rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
+elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
+  # Someone else has the file, wait till it's released
+  while [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; do
+    echo " There seems to be a download currently under way, will check again in 5 sec"
+    sleep 5
+  done
+else
+  echo "continuing from previous builds, using source at " ${SRC_DIR}/${SOURCE_FILE}
+fi
+tar xjf  ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
+mkdir -p ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+../configure \
+--prefix=${SOFT_DIR} \
+--with-blas \
+--with-lapack \
+make -j 2
