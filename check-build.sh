b3f34028825a29e5e613a02a787c3e19adce5ae7
Bruce Becker <brucellino@gmail.com>
2016-11-24 12:26:28 +0100

    added the check and deploy scripts

---
+++ check-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+module load ci
+cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+make check
+
+echo $?
+
+make install
+mkdir -p ${REPO_DIR}
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       GMP_VERSION       $VERSION
+setenv       GMP_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
+prepend-path CFLAGS            "-I${GMP_DIR}/include"
+prepend-path LDFLAGS           "-L${GMP_DIR}/lib"
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+cp modules/$VERSION ${LIBRARIES_MODULES}/${NAME}
---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+# this should be run after check-build finishes.
+. /etc/profile.d/modules.sh
+echo ${SOFT_DIR}
+module add deploy
+echo ${SOFT_DIR}
+cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+../configure ABI=64 \
+--with-gnu-ld \
+--enable-shared \
+--prefix=${SOFT_DIR}
+make install -j2
+echo "Creating the modules file directory ${LIBRARIES_MODULES}"
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/gmp-deploy"
+setenv GMP_VERSION       $VERSION
+setenv GMP_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(GMP_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(GMP_DIR)/include
+prepend-path CFLAGS            "-I${GMP_DIR}/include"
+prepend-path LDFLAGS           "-L${GMP_DIR}/lib"
+MODULE_FILE
+) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
