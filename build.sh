059a5fe641ce11f71802c023f0d9312a68464467
Bruce Becker <brucellino@gmail.com>
2017-06-08 17:04:05 +0200

    add ldpath

--- build.sh
+++ build.sh
@@ -46,8 +46,8 @@ fi
 tar xfz  ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
 mkdir -p ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
 cd ${WORKSPACE}/${NAME}-${VERSION}/build-${BUILD_NUMBER}
-CFLAGS="$CFLAGS -I$BZLIB_DIR/include" \
-LDFLAGS="-L${BZLIB_DIR}/lib" \
+CFLAGS="$CFLAGS -I$BZLIB_DIR/include -I${LZ_DIR}/include" \
+LDFLAGS="-L${BZLIB_DIR}/lib -L${XZ_DIR}/lib" \
 ../configure \
 --build=x86_64-pc-linux-gnu \
 --host=x86_64-pc-linux-gnu \
